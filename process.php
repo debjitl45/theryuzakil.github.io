0d4da55ff5c33a8f8f602ee4c6a32e7614a9a5ff
DEBJIT <karmadebjit@gmail.com>
2019-06-19 21:00:26 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+$mysqli = new mysqli('localhost','root','','data') or die(mysqli_error($mysqli));
+
+$name = '';
+$budget = '';
+$update = false;
+
+if(isset($_POST['save'])){
+	$name = $_POST['name'];
+	$budget = $_POST['budget'];
+	$mysqli->query("INSERT INTO data(name, budget) VALUES('$name','$budget')") or die($mysqli->error);
+
+	$_SESSION['message']  = "Record has been saved!";
+	$_SESSION['msg_type'] = "success";
+	header("location: money.php");
+}
+if(isset($_GET['delete'])){
+	$id = $_GET['delete'];
+	$mysqli->query("DELETE FROM data WHERE id='$id'") or die($mysqli->error());
+
+	$_SESSION['message']  = "Record has been deleted!";
+	$_SESSION['msg_type'] = "danger";
+	header("location: money.php");
+}
+
+if(isset($_GET['edit'])){
+	$id = $_GET['edit'];
+	$update = true;
+	$result = $mysqli->query("SELECT * FROM data WHERE id='$id' ") or die($mysqli->error());
+	if($result)
+	{
+		$row = $result->fetch_array();
+		$name = $row['name'];
+		$budget = $row['budget'];
+	}
+
+}
+header("location: money.php");
+
+?>
